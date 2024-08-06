807e84c4b243a7931653707c5ac87d7e1d7594ee
alyssaf16 <alyssa_feinberg@brown.edu>
2024-08-06 14:54:44 -0400

    all

--- core/phoneme_processing.py
+++ core/phoneme_processing.py
@@ -15,7 +15,6 @@ from transformers import DonutProcessor, VisionEncoderDecoderModel
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from transformers import LayoutLMv2Processor, LayoutLMv2ForTokenClassification
 from .audio_utils import load_audio, parse_words
-from run import app
 from dotenv import load_dotenv
 
 def recognize_speech_logic(url):
