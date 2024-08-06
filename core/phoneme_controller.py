b9043c53ce7b0b77c3042e1cfefc6aef9530a222
alyssaf16 <alyssa_feinberg@brown.edu>
2024-08-06 14:10:42 -0400

    all

--- core/phoneme_controller.py
+++ core/phoneme_controller.py
@@ -1,9 +1,9 @@
 from flask import request, jsonify
-from .phoneme_processing import recognize_speech_logic
 from run import app
 
 @app.route('/labels/', methods=['POST'])
 def recognize_speech():
+    from .phoneme_processing import recognize_speech_logic
     data = request.get_json()
     url = data['file']
     response = recognize_speech_logic(url)
